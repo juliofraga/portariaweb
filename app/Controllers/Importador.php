38e119c3271f04eb8f5592e2ed1fb60a2c46fcbd
Julio Fraga <julio_fraga10@hotmail.com>
2023-10-30 08:47:28 -0300

    Importação da biblioteca simplexlsx para leitura do importador

--- app/Controllers/Importador.php
+++ app/Controllers/Importador.php
@@ -1,14 +1,18 @@
 <?php
 
+    use Shuchkin\SimpleXLSX;
+
     class Importador extends Controller{
 
         public $helper;
         public $log;
+        public $SimpleXLSX;
 
         public function __construct()
         {
             $this->helper = new Helpers();
             $this->log = new Logs();
+            require "../public/vendor/simplexlsx/src/SimpleXLSX.php";
         }
 
         public function index()
@@ -24,7 +28,12 @@
         public function importaArquivo(){
             if($this->helper->sessionValidate()){
                 $temp_file = $_FILES["arquivo"]["tmp_name"];
-                
+                $arquivo = SimpleXLSX::parse($temp_file);
+                //var_dump($arquivo->rows());
+                for($i = 0; $i < count($arquivo->rows()); $i++){
+                    var_dump($arquivo->rows()[$i]);
+                    echo "<br><br><br>";
+                }
             }else{
                 $this->helper->redirectPage("/login/");
             } 
